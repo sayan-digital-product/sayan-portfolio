01eb3f3793c2ded1f2d603514a0c4203e6c7d929
sayan-banerjee87 <61361944+sayan-banerjee87@users.noreply.github.com>
2024-08-25 23:41:14 +0530

    added clarity tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { getInitColorSchemeScript } from '@mui/material/styles';
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -8,6 +9,15 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" type="image/ico" sizes="500x500"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
         <meta name="msapplication-TileImage" content="/favicon.png" />
+        <Script id="microsoft-clarity-analytics">
+          {`
+            (function(c,l,a,r,i,t,y){
+                    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+                })(window, document, "clarity", "script", "nsxfosed8s");
+          `}
+        </Script>
       </Head>
       <body>
         {getInitColorSchemeScript()}
