10544b61482af95b1a3d0ea045f89e34270a171b
sayan-banerjee87 <61361944+sayan-banerjee87@users.noreply.github.com>
2024-08-26 11:48:08 +0530

    added script tag for clarity integration

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,15 +9,16 @@ export default function Document() {
         <link rel="icon" href="/favicon.ico" type="image/ico" sizes="500x500"/>
         <link rel="apple-touch-icon" sizes="180x180" href="/favicon-180x180.png" />
         <meta name="msapplication-TileImage" content="/favicon.png" />
-        <Script id="microsoft-clarity-analytics">
-          {`
-            (function(c,l,a,r,i,t,y){
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "nsxfosed8s");
-          `}
-        </Script>
+                })(window, document, "clarity", "script", "nsxfosed8s");`,
+          }}
+        />
       </Head>
       <body>
         {getInitColorSchemeScript()}
